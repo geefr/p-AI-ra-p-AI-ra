71b535a0e091d7ce31fd8e38a43f8aced5e63201
Gareth Francis <gfrancis.dev@gmail.com>
2022-02-21 20:40:14 +0000

    Make it work >:[

--- main.py
+++ main.py
@@ -12,10 +12,21 @@ from PIL import Image, ImageDraw
 from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
-VIDEO_WIDTH = 800
-VIDEO_HEIGHT = 600
-ENABLE_PREVIEW = False
+VIDEO_WIDTH = 640
+VIDEO_HEIGHT = 480
 ENABLE_OVERHEAD_VIEW = False
+ENABLE_PREVIEW = True
+
+# Enable to log angles
+# Stand with chest forward, each arm to front / side in turn
+# Adjust offsets to they swepp 0 - 90 degrees, or as close as you can get
+CALIBRATION_MODE = False
+angle_offset_left = 0
+angle_offset_right = 30
+# The minimum distance away from center of body to count as active
+min_wrist_distance = 0.2
+input_mash_time = 0.05
+arm_level_thresh = 0.4
 
 up = glm.vec3(0.0, 1.0, 1.0)
 
@@ -29,6 +40,11 @@ def calc_forward_vec(left_hip, right_hip):
     # print(f"Forward: {forward_vec}")
     return forward_vec
 
+def is_arm_level(shoulder_pos, arm_pos):
+    # Threshold height from shoulder (this is dumb, use angles?)
+    d = abs(arm_pos.y - shoulder_pos.y)
+    return d < arm_level_thresh
+        
 def calc_arm_angle(forward_vec, arm_vec):
     try:
         forward_vec = copy(forward_vec)
@@ -57,6 +73,7 @@ class ButtonState:
         self.left_arm = False
         self.right_arm = False
         self.pressed = False
+        self.toggle_time = time.time()
     
     def __repr__(self) -> str:
         return f"LEFT: {self.left_arm} RIGHT: {self.right_arm} PRESSED: {self.pressed}"
@@ -78,11 +95,11 @@ class AngleRange:
 
 def press_buttons(left_angle, right_angle, button_state):
     button_angles = {
-        'z': AngleRange(-90, -60),
+        'z': AngleRange(-150, -50),
         'x': AngleRange(-60, -30),
-        'c': AngleRange(-30, 30),
+        'c': AngleRange(-40, 40),
         'v': AngleRange(30, 60),
-        'b': AngleRange(60, 90),
+        'b': AngleRange(50, 150),
     }
 
     # print(f"press_buttons: {left_angle}, {right_angle}")
@@ -102,20 +119,38 @@ def press_buttons(left_angle, right_angle, button_state):
         else:
             state.right_arm = False
 
-   #  print(f"0: {button_state['x']}")
+    #  print(f"0: {button_state['x']}")
+    now = time.time()
     for (button, state) in button_state.items():
         if state.left_arm or state.right_arm:
-            if not state.pressed:
+            if not state.pressed and (now - state.toggle_time) > input_mash_time:
                 state.pressed = True
+                state.toggle_time = now
                 keyboard.press(button)
-                print(f"PRESS {button}")
+                # print(f"PRESS {button}")
+            elif (now - state.toggle_time) > input_mash_time:
+                state.pressed = False
+                state.toggle_time = now
+                keyboard.release(button)
         else:
             if state.pressed:
                 state.pressed = False
+                state.toggle_time = now
                 keyboard.release(button)
-                print(f"RELEASE {button}")
+                # print(f"RELEASE {button}")
                 
     # print(f"1: {button_state['x']}")
+
+def print_state(left_angle, right_angle, button_state):
+    msg = ""
+    msg += f"{int(left_angle):5} "
+    for s in button_state.values():
+        if s.pressed:
+            msg += "x"
+        else:
+            msg += "_"
+    msg += f" {int(right_angle):5}"
+    print(msg)
 
 def draw_overhead_view(poses):
     world_min = glm.vec3(-0.5, -0.5, -0.5)
@@ -162,7 +197,7 @@ def main():
     mp_pose = mp.solutions.pose
     pose = mp_pose.Pose(
         model_complexity=2,
-        enable_segmentation=False,  # Generate a segmentation mask - don't need it here
+        enable_segmentation=True,  # Generate a segmentation mask
         min_detection_confidence=0.9,
         min_tracking_confidence=0.9
     )
@@ -170,7 +205,8 @@ def main():
     mp_draw = mp.solutions.drawing_utils
     mp_draw_styles = mp.solutions.drawing_styles
 
-    first_frame_gray = None
+    bg_image = None
+    first_frame = True
 
     print("Begin Main Loop")
     done = False
@@ -178,18 +214,16 @@ def main():
         ret, frame = cap.read()
         if not ret:
             print("ERROR: Failed to read frame")
-            continue
-            #done = True
-            #break
+            done = True
+            break
+
+        if first_frame:
+            bg_image = np.zeros(frame.shape, dtype=np.uint8)
+            bg_image[:] = (192, 192, 192)
+            first_frame = False
+
         # Output frame to display/annotate
         orig_frame = deepcopy(frame)
-
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.GaussianBlur(frame_gray, (21, 21), 0)
-
-        if first_frame_gray is None:
-            first_frame_gray = deepcopy(frame_gray)
-
         frame_rgb = cv2.cvtColor(orig_frame, cv2.COLOR_BGR2RGB)
 
         landmarks = {
@@ -216,24 +250,32 @@ def main():
                 print("Invalid pose data - Missing a landmark")
                 continue
 
-            forward_vec = calc_forward_vec(poses['left_hip'], poses['right_hip'])
+            # forward_vec = calc_forward_vec(poses['left_hip'], poses['right_hip'])
+            forward_vec = calc_forward_vec(poses['left_shoulder'], poses['right_shoulder'])
 
-            thresh = 0.2
-            if abs(poses['left_wrist'].x) < thresh and abs(poses['left_wrist'].z) < thresh:
+            if abs(poses['left_wrist'].x) < min_wrist_distance and abs(poses['left_wrist'].z) < min_wrist_distance:
+                left_angle = -180
+            elif not is_arm_level(poses['left_shoulder'], poses['left_wrist']):
                 left_angle = -180
             else:
                 left_arm_vec = poses['left_wrist'] - poses['left_shoulder']
                 left_angle = calc_arm_angle(forward_vec, left_arm_vec)
+                left_angle += angle_offset_left
 
-            if abs(poses['right_wrist'].x) < thresh and abs(poses['right_wrist'].z) < thresh:
+            if abs(poses['right_wrist'].x) < min_wrist_distance and abs(poses['right_wrist'].z) < min_wrist_distance:
+                right_angle = -180
+            elif not is_arm_level(poses['right_shoulder'], poses['right_wrist']):
                 right_angle = -180
             else:
                 right_arm_vec = poses['right_wrist'] - poses['right_shoulder']
                 right_angle = calc_arm_angle(forward_vec, right_arm_vec)
+                right_angle += angle_offset_right
 
-            print(f"LEFT: {left_angle} RIGHT: {right_angle}")
+            # xprint(f"LEFT: {left_angle} RIGHT: {right_angle}")
 
             press_buttons(left_angle, right_angle, button_state)
+            if CALIBRATION_MODE:
+                print_state(left_angle, right_angle, button_state)
 
             if ENABLE_OVERHEAD_VIEW:
                 draw_overhead_view(poses)
@@ -241,14 +283,12 @@ def main():
         if ENABLE_PREVIEW:
             # Preview window
             if pose_results.pose_landmarks is not None:
-                for (mark, id) in landmarks.items():
-                    mark_pos = pose_results.pose_landmarks.landmark[id]
-
-                    height, width, channels = frame.shape
-                    x = int(mark_pos.x * width)
-                    y = int(mark_pos.y * height)
-
-                    cv2.circle(frame, (x,y), 10, (255, 0, 255), cv2.FILLED)
+                # for (mark, id) in landmarks.items():
+                #     mark_pos = pose_results.pose_landmarks.landmark[id]
+                #     height, width, channels = frame.shape
+                #     x = int(mark_pos.x * width)
+                #     y = int(mark_pos.y * height)
+                #     cv2.circle(frame, (x,y), 10, (255, 0, 255), cv2.FILLED)
 
                 mp_draw.draw_landmarks(
                     frame,
@@ -257,6 +297,15 @@ def main():
                     landmark_drawing_spec=mp_draw_styles.get_default_pose_landmarks_style()
                 )
 
+            if pose_results.segmentation_mask is not None:
+                # Draw segmentation on the image.
+                # To improve segmentation around boundaries, consider applying a joint
+                # bilateral filter to "results.segmentation_mask" with "image".
+                condition = np.stack((pose_results.segmentation_mask,) * 3, axis=-1) > 0.1
+                frame = np.where(condition, frame, bg_image)
+            else:
+                frame = bg_image
+            
             cv2.namedWindow(annotation_window)
             cv2.imshow(annotation_window, frame)
 
